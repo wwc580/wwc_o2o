cf9cc0b20a8fc938c20cb487b665f1f136be4524
wwc580 <532591407@qq.com>
2017-08-20 16:38:21 +0800

    商家模块model代码复用编写 和 注册页跳转页面

--- application/common/model/BisLocation.php
+++ application/common/model/BisLocation.php
@@ -3,23 +3,7 @@ namespace app\common\model;
 
 use think\Model;
 
-class BisLocation extends Model
+class BisLocation extends BaseModel
 {
-    //自动记录时间为真    或者在database.php中设置auto_timestamp 为真
-    protected $autoWriteTimestamp = true;
-
-    /**
-     * 新增分类
-     * @param $data
-     * @return false|int
-     */
-    public function add($data)
-    {
-        $data['status'] = 0;
-        //$data['create_time'] = time();
-        $this->save($data);
-        //返回主键id
-        return $this->id;
-    }
 
 }
